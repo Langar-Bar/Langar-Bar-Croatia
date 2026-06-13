ecca3f0b3c1848c1086cdda3c80e00722718c28f
Langar-Bar <langaarbar@gmail.com>
2026-06-13 03:04:19 +0200

    Delete v426 directory

--- v426/service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME = 'langar-bar-v4-2-4-club-login-customers-rpc';
-const ASSETS = ['./','./index.html','./admin.html','./styles.css','./manifest.webmanifest','./admin-manifest.webmanifest','./js/menu-data.js','./js/app.js','./js/cloud.js','./js/admin.js','./js/admin-cloud.js','./assets/logo.jpeg','./assets/icon-192.png','./assets/icon-512.png','./assets/admin-icon-192.png','./assets/admin-icon-512.png'];
-self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(()=>self.skipWaiting())); });
-self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
-self.addEventListener('fetch', event => { if(event.request.method !== 'GET') return; event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).catch(()=>cached))); });
