bc9abf9c3885f0c50780fa14883f1ec6670f88d5
Langar-Bar <langaarbar@gmail.com>
2026-07-13 23:27:00 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'langar-v540';
+const CACHE_NAME = 'langar-v541';
 const ASSETS = ['./','./index.html','./admin.html','./styles.css','./manifest.webmanifest','./admin-manifest.webmanifest','./js/menu-data.js','./js/app.js','./js/cloud.js','./js/auth-v500.js','./privacy.html','./terms.html','./js/admin.js','./js/admin-cloud.js','./js/admin-stable-v433.js','./js/order-print-v510.js','./README_V510_START_HERE.md','./assets/logo.jpeg','./assets/icecream_cone.svg','./assets/icon-192.png','./assets/icon-512.png','./assets/admin-icon-192.png','./assets/admin-icon-512.png','js/stabilization-v520.js'
 ,'js/admin-stabilization-v520.js'
 ,'js/order-print-v520.js'
@@ -10,10 +10,12 @@ const ASSETS = ['./','./index.html','./admin.html','./styles.css','./manifest.we
 'js/admin-fixes-v532.js',
 'js/client-fixes-v532.js',
 'css/production-fixes-v532.css',
-  'css/knowledge-v540.css',
-  'js/admin-knowledge-v540.js',
-  'js/client-knowledge-v540.js'
-];
+  'css/knowledge-v541.css',
+  'js/admin-knowledge-v541.js',
+  'js/client-knowledge-v541.js'
+,
+  './js/admin-knowledge-fixes-v541.js',
+  './css/knowledge-fixes-v541.css'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(()=>self.skipWaiting())); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
 self.addEventListener('fetch', event => {
